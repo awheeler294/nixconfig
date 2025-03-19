f4eba750fee4a5997b9f2eb7489808add84f5c0c
Andrew Wheeler <awheeler294@gmail.com>
2025-03-19 12:04:20 -0600

    add shoebox

--- flake.nix
+++ flake.nix
@@ -82,6 +82,31 @@
       ];
     };
 
+    nixosConfigurations.shoebox-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./shoebox/configuration.nix
+
+	# make home-manager as a module of nixos
+        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.andrew = import ./home/andrew.nix;
+          home-manager.users.kid = import ./home/kid.nix;
+	  home-manager.extraSpecialArgs = {
+	    inherit inputs;
+	  };
+          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
+    };
+
     nixosConfigurations.vaulty-server = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };

d2d3d381316cdee1d9133f2a259eab4237e1792f
Andrew Wheeler <awheeler294@gmail.com>
2026-04-16 14:56:49 -0600

    makemkv alias

--- the-forest/home.nix
+++ the-forest/home.nix
@@ -10,6 +10,10 @@
         ../home/nvim.nix
         ../home/zsh.nix
       ];
+
+      home.shellAliases = {
+        makemkv = "NIXPKGS_ALLOW_UNFREE=1 nix-shell --run makemkv -p makemkv -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/2c36ece932b8c0040893990da00034e46c33e3e7.tar.gz";
+      };
     };
 
     kid = import ../home/kid.nix;

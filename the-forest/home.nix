f055a01a467711dde274705fd5b04d2f0b8c5a0f
Andrew Wheeler <awheeler294@gmail.com>
2026-05-03 16:54:38 -0600

    shortcut for MakeMkv version that works

--- the-forest/home.nix
+++ the-forest/home.nix
@@ -14,6 +14,19 @@
       home.shellAliases = {
         makemkv = "NIXPKGS_ALLOW_UNFREE=1 nix-shell --run makemkv -p makemkv -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/2c36ece932b8c0040893990da00034e46c33e3e7.tar.gz";
       };
+
+      xdg.desktopEntries = {
+        makemkv = {
+          name = "MakeMkv";
+          genericName = "Disk Archiver";
+          exec = "bash -c \"NIXPKGS_ALLOW_UNFREE=1 nix-shell --run 'systemd-inhibit makemkv' -p makemkv -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/2c36ece932b8c0040893990da00034e46c33e3e7.tar.gz\"";
+          terminal = false;
+        };
+      };
+
+      programs.rofi = {
+        enable = true;
+      };
     };
 
     kid = import ../home/kid.nix;
